39f6e20836157977945d2d0b76d3ca182f8b3876
homulko <106237058+homulko@users.noreply.github.com>
2022-12-20 15:28:34 +0200

    Set up project (#2)
    * configure joi validation, orm and nest infrastracture
    
    * add app module
    
    * add prettier configs, rename dto file
    
    * format code with last prettier changes

---
+++ src/app/pipes/validation/joiValidation/types.ts
@@ -0,0 +1,10 @@
+import Joi from 'joi'
+
+export interface IValidationError {
+  message: string
+  property: string[]
+}
+
+export type TJoiValidationSchemas = {
+  [key: string]: Joi.ObjectSchema | Joi.AlternativesSchema
+}
