1862fe5583fc9cbae3086629abeb41a1116a5c83
homulko <vladimir.homulko@menklab.com>
2022-12-30 15:31:55 +0200

    Add util function to convert errors to ApiExeption

--- src/common/errors/utils/business-exception.ts
+++ src/common/errors/utils/business-exception.ts
@@ -1,20 +1,10 @@
+import {IError} from "../interfaces/error.interface";
 import {ApiException} from "../interfaces/api-exception.interface";
 import {ErrorType} from "../enum/error-type";
-import {ERRORS} from "../errors";
 
-export class BusinessException {
-
-  public static invalidMessage(): ApiException {
-    return {
-      type: ErrorType.BUSINESS_ERRORS,
-      errors: [ERRORS.auth.invalidMessage],
-    }
-  }
-
-  public static unauthorized(): ApiException {
-    return {
-      type: ErrorType.BUSINESS_ERRORS,
-      errors: [ERRORS.auth.notAuthorized],
-    }
+export function businessException(errors: IError[]): ApiException {
+  return {
+    type: ErrorType.BUSINESS_ERRORS,
+    errors
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,8 +4,9 @@ import nacl from 'tweetnacl'
 import { UserService } from '../user/user.service'
 import { AuthInputDto } from './dtos/auth/auth.input.dto'
 import * as crypto from 'crypto'
-import {ERRORS, ErrorType} from '../../common'
-import {BusinessException} from "../../common/errors/utils/business-exception";
+import {businessException} from "../../common/errors/utils/business-exception";
+import {ERRORS} from "../../common";
+
 
 @Injectable()
 export class AuthService {
@@ -19,7 +20,7 @@ export class AuthService {
 
   public async verifyMessage(session: Record<string, any>, { pubKey, message, signature }: AuthInputDto): Promise<boolean> {
     if (session.message !== message) {
-      throw new BadRequestException(BusinessException.invalidMessage())
+      throw new BadRequestException(businessException([ERRORS.auth.invalidMessage]))
     }
 
     const encodedMessage = new TextEncoder().encode(message)
@@ -33,7 +34,7 @@ export class AuthService {
     const isAuthorized = nacl.sign.detached.verify(encodedMessage, signatureUint8Array, bs58.decode(pubKey))
 
     if (!isAuthorized) {
-      throw new UnauthorizedException(BusinessException.unauthorized())
+      throw new UnauthorizedException(businessException([ERRORS.auth.notAuthorized]))
     }
 
     let user = await this.userService.findByPubKey(pubKey)
