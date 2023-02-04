aa1086072d0e76c8602ca96d3c5889c89090f211
Stanislav <stanislav@Stanislavs-MacBook-Pro.local>
2023-02-03 19:39:20 -0500

    DRAC-342 | added auth custom error

--- src/modules/app/guards/auth.guard.ts
+++ src/modules/app/guards/auth.guard.ts
@@ -1,9 +1,16 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import { CanActivate, ExecutionContext, Injectable, NotFoundException } from '@nestjs/common'
+import { businessException } from 'src/common/errors/utils/business-exception'
+import { ERRORS } from 'src/common'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const { session } = context.switchToHttp().getRequest()
-    return !!session.isAuthorized
+
+    if (!session.isAuthorized) {
+      throw new NotFoundException(businessException([ERRORS.auth.notAuthorized]))
+    }
+
+    return true
   }
 }
